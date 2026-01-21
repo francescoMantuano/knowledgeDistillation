a7cb97627823b8fbd828b28858aa247a9f1de834
francescoMantuano <francesco.mantuano@edu.unife.it>
2026-01-22 00:04:51 +0100

    modifica a batch_size, temperature e patience per provare

--- config.py
+++ config.py
@@ -2,17 +2,17 @@ import torch
 
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 NUM_CLASSES = 120
-BATCH_SIZE = 64
+BATCH_SIZE = 128 #64 prima
 #conviene fare un batch size sperato per teacher/student?
 NUM_EPOCHS = 30
-LR = 3e-4
+LR = 3e-4 #1e-4 prima
 WEIGHT_DECAY = 1e-4 #dropout per regolarizzare
 
 IMAGE_SIZE = 224
 
-KD_TEMPERATURE = 4.0 #eventualmente modificabile
+KD_TEMPERATURE = 1 #4.0 prima #eventualmente modificabile
 KD_ALPHA = 0.7 #eventualmente modificabile
 
-PATIENCE = 50 #early stopping per regolarizzare
+PATIENCE = 7 #early stopping per regolarizzare
 
     

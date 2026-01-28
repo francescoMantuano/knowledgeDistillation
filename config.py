0727e3e5dbaf122e2a45e9e2bcb548d0c2fa30ac
francescoMantuano <francesco.mantuano@edu.unife.it>
2026-01-28 17:56:52 +0100

    temperature = 3 e alpha = 0.6

--- config.py
+++ config.py
@@ -12,8 +12,8 @@ WEIGHT_DECAY = 1e-4 #per regolarizzare
 
 IMAGE_SIZE = 224
 
-KD_TEMPERATURE = 4 #eventualmente modificabile
-KD_ALPHA = 0.3 #eventualmente modificabile
+KD_TEMPERATURE = 3 #eventualmente modificabile
+KD_ALPHA = 0.6 #eventualmente modificabile
 
 PATIENCE = 15 #early stopping per regolarizzare
 

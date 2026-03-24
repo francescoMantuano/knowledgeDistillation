661b088463a1f3b9dcf32b842fd18e5a83818d73
francescoMantuano <francesco.mantuano@edu.unife.it>
2026-03-24 18:13:56 +0100

    aggiustato il modello student

--- models/student.py
+++ models/student.py
@@ -3,11 +3,8 @@ from torchvision import models
 from config import *
 
 def get_student(num_classes):
-    model = models.mobilenet_v2(weights="IMAGENET1K_V2") #carico dei pesi pre-addestrati sul database di ImageNet, una sorta di Transfer Learning. Altrimenti avrei valori random
-    model.classifier = nn.Sequential(
-        nn.Dropout(p=DROPOUT_RATE),     #dropout != da weight decay!!
-        nn.Linear(model.classifier[1].in_features, num_classes)
-    )
+    model = models.shufflenet_v2_x1_5(weights="IMAGENET1K_V1") #carico dei pesi pre-addestrati sul database di ImageNet, una sorta di Transfer Learning. Altrimenti avrei valori random
+    model.fc = nn.Linear(model.fc.in_features, num_classes)
     return model
 
 # come modelli student prendo shufflenet_v2_x1_5 e mobilenet_v2 perchè hanno un simile numero di parametri (circa 3.5 milioni), altrimenti mobilenet_v3_large/small hanno, rispettivamente, circa 5.5/2.5 milioni di parametri e shufflenet_v2_x2_0 ha circa 7 milioni di parametri
